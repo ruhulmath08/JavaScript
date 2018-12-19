806810bb56982af1cf850f608fc2f2a6598519e9
unknown <ruhulmath08@gmail.com>
2018-12-19 16:36:59 +0600

    Add JavaScript Array

--- Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
+++ Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
@@ -1,3 +1,12 @@
+const numbers = [1, -1, 2, 3];
 
+//a=0; c=1 => a=1;
+//a=1, c=-1 => a=0
+//a=0, c=2, a=2
+//a=2, c=3, a=5
 
+const sum = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
 
+console.log(sum);
