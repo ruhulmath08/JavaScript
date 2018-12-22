c618c2216a3ca42ccdf34effd8123ff43262394b
Md. Ruhul AMin <ruhulmath08@gmail.com>
2018-12-23 01:46:04 +0600

    Add upto this keyword

--- Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
+++ Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
@@ -1,32 +1,18 @@
-const person = {
-  firstName: "Mosh",
-  lastName: "Hamedani",
-  get fullName() {
-    return `${person.firstName} ${person.lastName}`;
-  },
-  //value receive here is a valid string
-  set fullName(value) {
-    //if(typeof value !== 'string') return
-    if (typeof value !== "string") {
-      throw new Error("Value is not a string");
-    }
-    const parts = value.split(" ");
-    if (parts.length !== 2) {
-      throw new Error("Enter a valid name");
-    }
-    this.firstName = parts[0];
-    this.lastName = parts[1];
+
+const video ={
+  title: 'a',
+  tags: ['a', 'b', 'c'],
+  showTags(){
+    this.tags.forEach(function(tag){
+      console.log(this.title, tag);
+    }, this);
   }
 };
 
-try {
-  person.fullName = "";
-} catch (e) {
-  console.log(e);
-}
-console.log(person);
+video.showTags();
+
 /*
-Error: Enter a valid name
-    at Object.set fullName [as fullName]
-{firstName: "Mosh", lastName: "Hamedani"}
+a a
+a b
+a c
 */
