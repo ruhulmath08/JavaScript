7a5236e6354915f403248a864753d6c2d11f99e3
unknown <ruhulmath08@gmail.com>
2018-12-22 16:34:41 +0600

    Add JavaScript Function up to Error Handaling

--- Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
+++ Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
@@ -1,8 +1,32 @@
-const numbers = [1, 2, 3, 4];
-const max = getMax([1, 2, 3, 4, 5]);
-console.log(max);
+const person = {
+  firstName: "Mosh",
+  lastName: "Hamedani",
+  get fullName() {
+    return `${person.firstName} ${person.lastName}`;
+  },
+  //value receive here is a valid string
+  set fullName(value) {
+    //if(typeof value !== 'string') return
+    if (typeof value !== "string") {
+      throw new Error("Value is not a string");
+    }
+    const parts = value.split(" ");
+    if (parts.length !== 2) {
+      throw new Error("Enter a valid name");
+    }
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  }
+};
 
-function getMax(array) {
-  if (array.length === 0) return undefined;
-  return array.reduce((a, b) => (a > b ? current : b));
+try {
+  person.fullName = "";
+} catch (e) {
+  console.log(e);
 }
+console.log(person);
+/*
+Error: Enter a valid name
+    at Object.set fullName [as fullName]
+{firstName: "Mosh", lastName: "Hamedani"}
+*/
