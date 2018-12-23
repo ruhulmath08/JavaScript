87dbb2f67d141a1ddf4799b709df242c54c0eabc
Md. Ruhul AMin <ruhulmath08@gmail.com>
2018-12-24 01:54:27 +0600

    Make array Note

--- Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
+++ Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
@@ -1,22 +1,4 @@
-function countOccurrences(array, searchElement) {
-  if (!Array.isArray(array)) {
-    throw new Error("Not an array");
-  }
-  return array.reduce((accumulator, current) => {
-    const occurrence = current === searchElement ? 1 : 0;
-    return accumulator + occurrence;
-  }, 0);
-}
+const numbers = [1, 2, 3, 1, 5, 1];
+console.log(numbers.lastIndexOf(1)); //5
+console.log(numbers.lastIndexOf(1, 4)); //3
 
-try {
-  const numbers = [1, 2, 3, 4, 1];
-  const count = countOccurrences(100, 1);
-  console.log(count);
-} catch (e) {
-  console.log(e);
-}
-
-/*
-Error: Not an array
-    at countOccurrences
-*/
