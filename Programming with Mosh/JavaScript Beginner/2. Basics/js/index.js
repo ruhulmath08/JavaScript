0d252d492da4b6ca878e7b21268d4314fb4f9b6f
unknown <ruhulmath08@gmail.com>
2018-12-26 16:32:20 +0600

    Update Array-6 doc file up to 'Reduce an array-16'

--- Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
+++ Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
@@ -1,16 +1,20 @@
-let numbers = [1, 2, 3, 4];
-let another = numbers;
+const numbers = [1, -1, 2, 3];
 
-numbers.length = 0;
-console.log(numbers); //[]
-console.log(another); //[]
+//initially a(accumulator) = 0 and c(cuttentValue) = 1 (ist element)
+//a=0; c(1st element)=1 => a=1;
+//a=1, c(2nd element)=-1 => a=0
+//a=0, c(3rd element)=2, a=2
+//a=2, c(4th element)=3, a=5
 
+const sum = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
 
+console.log(sum); //5
 
+//or arrow function
 
-
-
-
- 
-
-
+const sum1 = numbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
+console.log(sum1); //5
