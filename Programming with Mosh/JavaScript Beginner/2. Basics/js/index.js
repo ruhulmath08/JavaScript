4f8f31b5d313c9622c46693963b64a7d0aaf2708
unknown <ruhulmath08@gmail.com>
2019-01-02 16:34:20 +0600

    add Basic, Operator and Control-flow

--- Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
+++ Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
@@ -1,14 +1,12 @@
-const movies = [
-  { title: "a", year: 2018, rating: 4.5 },
-  { title: "b", year: 2018, rating: 4.7 },
-  { title: "c", year: 2018, rating: 3 },
-  { title: "d", year: 2017, rating: 4.5 }
-];
-
-const result = movies
-  .filter(m => m.year === 2018 && m.rating >= 4) //All the movies in 2018 with rating > 4
-  .sort((a, b) => a.rating - b.rating) //Sort them by their rating
-  .reverse() //Descending order
-  .map(m => m.title); //Pick their title
-
-console.log(result); //["b", "a"]
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 5);
+/*
+1
+2
+3
+4
+5
+*/
