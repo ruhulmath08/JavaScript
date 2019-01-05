6944faef92b987de26541e25b35b5f09f6705577
unknown <ruhulmath08@gmail.com>
2019-01-05 16:25:41 +0600

    Object upto exercise-4

--- Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
+++ Programming with Mosh/JavaScript Beginner/2. Basics/js/index.js
@@ -1,12 +1,26 @@
-let i = 1;
-do {
-  console.log(i);
-  i++;
-} while (i <= 5);
-/*
-1
-2
-3
-4
-5
-*/
+function Address(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
+}
+
+let address1 = new Address("17/A", "Dhaka", "1700");
+let address2 = new Address("17/A", "Dhaka", "1700");
+let address3 = address1;
+
+function areEqual(address1, address2) {
+  return (
+    address1.street === address2.street &&
+    address1.city === address2.city &&
+    address1.zipCode === address2.zipCode
+  );
+}
+
+function areSame(address1, address2) {
+  return address1 === address2;
+}
+
+console.log(areEqual(address1, address2)); //true
+console.log(areSame(address1, address2)); //false
+console.log(areSame(address1, address3)); //true
+
