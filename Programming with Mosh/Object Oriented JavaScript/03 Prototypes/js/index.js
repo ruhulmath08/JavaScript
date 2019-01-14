c03acba0e902265619aa3df28683d4dc33b5d6e4
unknown <ruhulmath08@gmail.com>
2019-01-14 16:51:13 +0600

    Add upto Prototype vs. Instance Members

--- Programming with Mosh/Object Oriented JavaScript/03 Prototypes/js/index.js
+++ Programming with Mosh/Object Oriented JavaScript/03 Prototypes/js/index.js
@@ -1,9 +1,10 @@
-function Circle(radious){
-  this.radious = radious;
-  this.draw = function(){
-    console.log("draw");    
-  };
-}
+let array = [];
+//both are same
+console.log(Array.prototype);
+console.log(array.__proto__);
 
-const circle = new Circle(10);
-""
+/*
+[constructor: ƒ, concat: ƒ, copyWithin: ƒ, fill: ƒ, find: ƒ, …]
+
+[constructor: ƒ, concat: ƒ, copyWithin: ƒ, fill: ƒ, find: ƒ, …]
+*/
