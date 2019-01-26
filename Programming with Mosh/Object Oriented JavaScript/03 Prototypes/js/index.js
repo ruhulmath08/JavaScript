d4731a9258f862802e36df431449aaaa4e610f12
unknown <ruhulmath08@gmail.com>
2019-01-26 15:59:48 +0600

    05 ES6 Classes upto methodriding

--- Programming with Mosh/Object Oriented JavaScript/03 Prototypes/js/index.js
+++ Programming with Mosh/Object Oriented JavaScript/03 Prototypes/js/index.js
@@ -1,10 +1,26 @@
-class Circle {
-  draw() {
-    console.log(this);
+class Shape {
+  move() {
+    console.log("Move");
   }
 }
 
-const c = new Circle();
-const draw = c.draw;
-//function call
-draw(); //undefined
+class Circle extends Shape {
+  move() {
+    console.log("Move");
+  }
+}
+
+const c = new Circle("red", 10);
+console.log(c); //Circle {color: "red", radius: 10}
+/*
+Circle {color: "red", radius: 10}
+  color: "red"
+  radius: 10
+  __proto__: Shape
+    constructor: class Circle
+    draw: ƒ draw()
+    __proto__:
+      constructor: class Shape
+      move: ƒ move()
+      __proto__: Object
+*/
