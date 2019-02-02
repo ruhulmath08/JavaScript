3161862dadb35862458f7d4a2b798bf53cf6e061
unknown <ruhulmath08@gmail.com>
2019-02-02 16:51:39 +0600

    COmplete 05 ES6 Classes

--- Programming with Mosh/Object Oriented JavaScript/03 Prototypes/js/index.js
+++ Programming with Mosh/Object Oriented JavaScript/03 Prototypes/js/index.js
@@ -1,26 +1,35 @@
-class Shape {
-  move() {
-    console.log("Move");
+const _items = new WeakMap();
+
+class Stack {
+  constructor() {
+    _items.set(this, []);
+  }
+
+  push(obj) {
+    _items.get(this).push(obj);
+  }
+
+  pop() {
+    const items = _items.get(this);
+
+    if (items.length === 0) {
+      throw new Error("Stack is empty");
+    }
+
+    return items.pop();
+  }
+
+  peek() {
+    const items = _items.get(this);
+
+    if (items.length === 0) {
+      throw new Error("Stack is empty");
+    }
+
+    return items[items.length - 1];
+  }
+
+  get count() {
+    return _items.get(this).length;
   }
 }
-
-class Circle extends Shape {
-  move() {
-    console.log("Move");
-  }
-}
-
-const c = new Circle("red", 10);
-console.log(c); //Circle {color: "red", radius: 10}
-/*
-Circle {color: "red", radius: 10}
-  color: "red"
-  radius: 10
-  __proto__: Shape
-    constructor: class Circle
-    draw: ƒ draw()
-    __proto__:
-      constructor: class Shape
-      move: ƒ move()
-      __proto__: Object
-*/
